1a2ebc99c14ec26147c0497f5d9a69db0d147f83
AAMFDO <156763248+AAMFDO@users.noreply.github.com>
2024-03-05 19:45:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,13 +19,6 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/dbinitialize", async function (req, res) {
-  console.log("DB is getting initialized");
-  let data = await dbinitialize();
-
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(data));
-});
 // ============== Teacher Related endpoints ==============
 
 app.get("/listTeachers", async function (req, res) {
